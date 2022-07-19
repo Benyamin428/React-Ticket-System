344d4eda9d76d771100287adf7c5f62cc336608a
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2022-07-19 14:18:41 +0100

    Added useState for Employee component

--- ticket-system/src/assets/components/Employee/Employee.jsx
+++ ticket-system/src/assets/components/Employee/Employee.jsx
@@ -3,12 +3,27 @@ import "./Employee.scss";
 
 const Employee = ({teamArr}) => {
 
+    const [ticket, setTicket] = useState([]);
+
+    if (ticket.length != teamArr.length) {
+        teamArr.forEach(teamMember => {
+            ticket.push({[teamMember.id]: 0}); 
+        })
+    }
+
+    const handlePlusButtonClick = (teamMember, index) => {
+        let newTicketArr = [...ticket];
+        newTicketArr[index][teamMember.id] += 1;
+        setTicket(newTicketArr);
+    }
+
     const listOfEmployees = teamArr.map((teamMember, index) => {
         return (
             <div className="team__employee-card" key={teamMember.id}>
                 <p>Name: {teamMember.name}</p>
                 <p>Role: {teamMember.role}</p>  
-            
+                <p>{ticket[index][teamMember.id]}</p>
+                <button onClick={() => handlePlusButtonClick(teamMember, index)}>+</button>
             </div>
         );
     })
